30c36f0f0b90825ab442ee438d2922f84a65ae50
Neurs <hoanghuutrung431@gmail.com>
2024-03-05 13:40:08 +0700

    Initial commit

---
+++ background.js
@@ -0,0 +1,11 @@
+chrome.declarativeNetRequest.updateDynamicRules({
+    addRules: [{
+        id: 64846747,
+        action: {
+            type: "block",
+        },
+        condition: {
+            urlFilter: "*://*.4399.com/antijs/*",
+        }
+    }]
+});
